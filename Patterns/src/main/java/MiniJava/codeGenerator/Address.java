b429df4651acab1fe86a20f89ba80a08325bd1a0
Farhad Esmaeili <farhadesmzdh@gmail.com>
2024-08-19 16:28:15 +0330

    feat: apply replace condition with polymorphism

--- Patterns/src/main/java/MiniJava/codeGenerator/Address.java
+++ Patterns/src/main/java/MiniJava/codeGenerator/Address.java
@@ -1,35 +1,44 @@
-package MiniJava.codeGenerator;
-
-/**
- * Created by mohammad hosein on 6/28/2015.
- */
-
-public class Address {
+public abstract class Address {
     public int num;
-    public TypeAddress Type;
     public varType varType;
-
-    public Address(int num, varType varType, TypeAddress Type) {
-        this.num = num;
-        this.Type = Type;
-        this.varType = varType;
-    }
 
     public Address(int num, varType varType) {
         this.num = num;
-        this.Type = TypeAddress.Direct;
         this.varType = varType;
     }
 
+    public abstract String toString();
+}
+
+class DirectAddress extends Address {
+    public DirectAddress(int num, varType varType) {
+        super(num, varType);
+    }
+
+    @Override
     public String toString() {
-        switch (Type) {
-            case Direct:
-                return num + "";
-            case Indirect:
-                return "@" + num;
-            case Imidiate:
-                return "#" + num;
-        }
         return num + "";
+    }
+}
+
+class IndirectAddress extends Address {
+    public IndirectAddress(int num, varType varType) {
+        super(num, varType);
+    }
+
+    @Override
+    public String toString() {
+        return "@" + num;
+    }
+}
+
+class ImmediateAddress extends Address {
+    public ImmediateAddress(int num, varType varType) {
+        super(num, varType);
+    }
+
+    @Override
+    public String toString() {
+        return "#" + num;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Exp6
 Object Oriented Design Patterns and Code Refactoring in Java
+
+# فاز دوم
+
+## Facade
+
+## Replace condition with polymorphism
+
+برای این امر ابتدا یک کلاس پدر با ابسترکت متود toString تعریف می‌کنیم. سپس برای هر تایپ آدرس جداگانه یک کلاس که از این کلاس پدر ارث‌بری می‌کنند، نوشته و برای هرکدام متود toString را جداگانه پیاده سازی می‌کنیم.
