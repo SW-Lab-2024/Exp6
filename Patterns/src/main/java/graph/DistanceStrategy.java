4f89f30391aa7ba6ad78c315851ee7f51031421a
amiiirdara <daraieamirmahdi@gmail.com>
2024-08-20 12:24:20 +0330

    chore: reformatting the DistanceStrategy

--- Patterns/src/main/java/graph/DistanceStrategy.java
+++ Patterns/src/main/java/graph/DistanceStrategy.java
@@ -1,11 +1,5 @@
 package graph;
 
-import org.javatuples.Pair;
-
-import java.util.LinkedList;
-import java.util.Queue;
-
 public interface DistanceStrategy {
     void calculateDistance(Graph graph, Node source);
 }
-
